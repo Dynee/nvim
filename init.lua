108875c4401f723b31751d6b7608696bfe2808ff
Dynee <lucaspadden@gmail.com>
2026-02-03 21:58:21 -0500

    use monokai color scheme

--- init.lua
+++ init.lua
@@ -816,16 +816,14 @@ require('lazy').setup({
   },
 
   {
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    'tanvirtin/monokai.nvim',
+    name = 'monokai',
+    lazy = false,
+    priority = 1000,
     config = function()
-      require('github-theme').setup {
-        -- ...
-      }
+      require('monokai').setup {}
 
-      vim.cmd 'colorscheme github_dark'
+      vim.cmd 'colorscheme monokai'
     end,
   },
 
