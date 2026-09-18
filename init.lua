08733f803a8ecad0e420c923b7da8d36d72c23d0
Dynee <lucaspadden@gmail.com>
2026-09-18 18:28:17 -0400

    feat: update gitsigns config to show blame and add lualine

--- init.lua
+++ init.lua
@@ -64,6 +64,7 @@ vim.cmd.colorscheme("gruvbox")
 
 -- Gitsigns
 require("gitsigns").setup({
+  current_line_blame = true,
   on_attach = function(bufnr)
     local gitsigns = require("gitsigns")
 
@@ -129,4 +130,60 @@ require("gitsigns").setup({
     -- Text object
     map({ "o", "x" }, "ih", gitsigns.select_hunk)
   end,
+})
+
+-- Lualine
+require("lualine").setup({
+  options = {
+    icons_enabled = true,
+    theme = "auto",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    always_show_tabline = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+      refresh_time = 16, -- ~60fps
+      events = {
+        "WinEnter",
+        "BufEnter",
+        "BufWritePost",
+        "SessionLoadPost",
+        "FileChangedShellPost",
+        "VimResized",
+        "Filetype",
+        "CursorMoved",
+        "CursorMovedI",
+        "ModeChanged",
+      },
+    },
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = { "filename" },
+    lualine_x = { "encoding", "fileformat", "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {},
 })
